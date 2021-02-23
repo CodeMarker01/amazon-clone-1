f991dcffb1cd577bc0ca21836e76549518bd02fa
CodeMarker01 <tsquang398@gmail.com>
2021-02-23 16:45:04 +0700

    create & design login page

--- src/Login.js
+++ src/Login.js
@@ -1,24 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
+  //STATE
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //EVENT
+  const signIn = (e) => {
+    e.preventDefault();
+
+    // firebase login
+  };
+  const register = (e) => {
+    e.preventDefault();
+
+    // firebase register
+  };
+  //RETURN
   return (
     <div className="login">
-      <h1>This is login page</h1>
-      <img
-        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        alt=""
-        className="login__logo"
-      />
+      <Link to="/">
+        <img
+          src="http://pngimg.com/uploads/amazon/amazon_PNG6.png"
+          alt=""
+          className="login__logo"
+        />
+      </Link>
+
       <div className="login__container">
-        <h2>Sign-in</h2>
+        <h1>Sign-in</h1>
         <form action="">
-          <label htmlFor="email">E-mail</label>
-          <input type="email" name="email" id="email" />
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" />
-          <button type="submit">Sign In</button>
+          <h5>E-mail</h5>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="login__signInButton" type="submit">
+            Sign In
+          </button>
         </form>
+        <p>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+        <button className="login__registerButton">
+          Create your Amazon Account
+        </button>
       </div>
     </div>
   );
